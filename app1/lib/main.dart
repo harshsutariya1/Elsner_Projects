f664d4a2d083245a6fca3476fa4509536af80861
Harsh Sutariya <hr.sutariya1@gmail.com>
2023-06-07 22:37:26 +0530

    updated

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -3,11 +3,11 @@ import 'package:app1/signupScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(myApp());
+  runApp(MyApp());
 }
 
-class myApp extends StatelessWidget {
-  const myApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
