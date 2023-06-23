635b9c521ded40c6c2382f5623646c7b55bab47d
Harsh Sutariya <hr.sutariya1@gmail.com>
2023-06-23 11:20:46 +0530

    updated

--- elsnerdemoproject/lib/homeScreen2.dart
+++ elsnerdemoproject/lib/homeScreen2.dart
@@ -9,11 +9,91 @@ class HomeScreen2 extends StatefulWidget {
 }
 
 class _HomeScreen2State extends State<HomeScreen2> {
+  TextEditingController data = TextEditingController();
+  String showingData = "";
+  changetext(String balu) {
+    setState(() {
+      showingData = balu;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
         child: Scaffold(
-      body: Center(child: Text("Home Screen 2")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 300,
+              child: TextField(
+                controller: data,
+                onChanged: (value) => changetext(value),
+                decoration: const InputDecoration(
+                  labelText: "Data",
+                  border: OutlineInputBorder(),
+                  hintText: "Enter Data...",
+
+                  // floatingLabelBehavior: FloatingLabelBehavior.auto
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              showingData,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
     ));
+  }
+}
+
+class Dataclass {
+  String data = "elsner data";
+}
+
+class JsontoDart {
+  String? activity;
+  String? type;
+  int? participants;
+  int? price;
+  String? link;
+  String? key;
+  int? accessibility;
+
+  JsontoDart(
+      {this.activity,
+      this.type,
+      this.participants,
+      this.price,
+      this.link,
+      this.key,
+      this.accessibility});
+
+  JsontoDart.fromJson(Map<String, dynamic> json) {
+    activity = json['activity'];
+    type = json['type'];
+    participants = json['participants'];
+    price = json['price'];
+    link = json['link'];
+    key = json['key'];
+    accessibility = json['accessibility'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['activity'] = activity;
+    data['type'] = type;
+    data['participants'] = participants;
+    data['price'] = price;
+    data['link'] = link;
+    data['key'] = key;
+    data['accessibility'] = accessibility;
+    return data;
   }
 }
--- elsnerdemoproject/lib/main.dart
+++ elsnerdemoproject/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:elsnerdemoproject/Functionality/elsnerLogo.dart';
+import 'package:elsnerdemoproject/homeScreen2.dart';
 import 'package:flutter/material.dart';
 // import 'package:shared_preferences/shared_preferences.dart';
 
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: HomeScreen2(),
     );
   }
 }
