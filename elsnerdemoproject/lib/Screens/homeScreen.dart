0a443ed04fcc7bdea3264aa157bbfa0653cad7f9
Harsh Sutariya <hr.sutariya1@gmail.com>
2023-06-29 11:51:29 +0530

    Updated

--- elsnerdemoproject/lib/Screens/homeScreen.dart
+++ elsnerdemoproject/lib/Screens/homeScreen.dart
@@ -70,92 +70,100 @@ class _ListviewState extends State<Listview> {
         ),
 // ________________________________________________________________________________________
 // ________________________________________________________________________________________
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                width: size.width,
-                child: ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const ApiExample()));
-                  },
-                  icon: const Icon(Icons.api_rounded),
-                  label: const Text("API Example"),
+        body: SingleChildScrollView(
+          child: SizedBox(
+            // If your screen is getting blank after wrapping a Column widget with a 
+            //SingleChildScrollView, it could be due to the way the 
+            //SingleChildScrollView is handling the available space. By default, 
+            //the SingleChildScrollView takes up all the available space in 
+            //its parent widget, which may cause the child Column to shrink and appear empty.
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: size.width,
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const ApiExample()));
+                      },
+                      icon: const Icon(Icons.api_rounded),
+                      label: const Text("API Example"),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            SingleChildScrollView(
-              // scrollDirection: ,
-              child: Row(
-                children: [rowCards()],
-              ),
-            ),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  "Images",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                const SizedBox(
+                  height: 5,
                 ),
-              ),
-            ),
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
+                Row(
+                  children: [rowCards()],
                 ),
-                itemCount: images.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    margin: const EdgeInsets.all(8),
-                    // decoration: BoxDecoration(border: Border.all()),
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        ImageShowScreen(images[index])));
-                          },
-                          child: SizedBox(
-                            width: double.maxFinite,
-                            height: double.maxFinite,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child: Opacity(
-                                opacity: 0.3,
-                                child: Image.asset(
-                                  "assets/${images[index]}",
-                                  fit: BoxFit.cover,
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      "Images",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: GridView.builder(
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                    ),
+                    itemCount: images.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        margin: const EdgeInsets.all(8),
+                        // decoration: BoxDecoration(border: Border.all()),
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            ImageShowScreen(images[index])));
+                              },
+                              child: SizedBox(
+                                width: double.maxFinite,
+                                height: double.maxFinite,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(20),
+                                  child: Opacity(
+                                    opacity: 0.3,
+                                    child: Image.asset(
+                                      "assets/${images[index]}",
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
+                            Text(
+                              "${images[index]}",
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
                         ),
-                        Text(
-                          "${images[index]}",
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
@@ -202,11 +210,14 @@ Widget rowCards() {
       border: Border.all(),
     ),
     child: Column(children: [
-      Image.asset(
-        "assets/car.jpg",
-        height: 150,
-        width: 250,
-        fit: BoxFit.cover,
+      ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Image.asset(
+          "assets/car.jpg",
+          height: 150,
+          width: 250,
+          fit: BoxFit.cover,
+        ),
       ),
       const SizedBox(
         height: 5,
@@ -254,25 +265,37 @@ Widget rowCards() {
           Padding(
             padding: const EdgeInsets.only(left: 8),
             child: SizedBox(
-              height: 30,width: 30,
-              child: Image.asset("assets/lock1.png",),
+              height: 30,
+              width: 30,
+              child: Image.asset(
+                "assets/lock1.png",
               ),
-              
+            ),
           ),
-          const SizedBox(width: 3,),
+          const SizedBox(
+            width: 3,
+          ),
           const Text("Car Services"),
-          const SizedBox(width: 35,),
+          const SizedBox(
+            width: 35,
+          ),
           const Text("From: "),
           Container(
-            padding: const EdgeInsets.only(left: 7,right: 7,top: 5,bottom: 5),
+            padding:
+                const EdgeInsets.only(left: 7, right: 7, top: 5, bottom: 5),
             decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 170, 75, 68),
-              borderRadius: BorderRadius.circular(5)
+                color: const Color.fromARGB(255, 170, 75, 68),
+                borderRadius: BorderRadius.circular(5)),
+            child: const Text(
+              "\$100",
+              style: TextStyle(color: Colors.white),
             ),
-            child: const Text("\$100",style: TextStyle(color: Colors.white),),
           )
         ],
       )
     ]),
   );
 }
+// ________________________________________________________________________________________
+// ________________________________________________________________________________________
+// ________________________________________________________________________________________
--- elsnerdemoproject/lib/main.dart
+++ elsnerdemoproject/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:elsnerdemoproject/Functionality/elsnerLogo.dart';
+// import 'package:elsnerdemoproject/Functionality/elsnerLogo.dart';
 // import 'package:elsnerdemoproject/homeScreen2.dart';
 import 'package:flutter/material.dart';
 // import 'package:shared_preferences/shared_preferences.dart';
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     );
@@ -55,7 +54,7 @@ class _SplashScreenState extends State<SplashScreen> {
             ],
           ),
         ),
-        child: const Center(child: ElsnerLogo()),
+        child: Center(child: elsnerLogo()),
       ),
     ));
   }
@@ -67,108 +66,114 @@ class _SplashScreenState extends State<SplashScreen> {
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
 
+  
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const ElsnerLogo(),
-              const SizedBox(
-                height: 100,
-              ),
-              const Text(
-                "Welcome!\n\nLet's find you some services.",
-                style: TextStyle(fontSize: 20),
-              ),
-              const SizedBox(
-                height: 100,
-              ),
-// __________________________________________________________________________________________
-// _________________________________Buttons__________________________________________________
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const LoginScreen()),
-                      );
-                    },
-                    child: Expanded(
-                      child: Container(
-                        alignment: Alignment.center,
-                        height: 50,
-                        width: 100,
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.red.withOpacity(0.25),
-                                blurRadius: 5,
-                                blurStyle: BlurStyle.normal,
-                                spreadRadius: 5)
-                          ],
-                          color: const Color.fromARGB(255, 255, 7, 7),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: const Text(
-                          "Login",
-                          style: TextStyle(
-                              fontSize: 15,
-                              color: Color.fromARGB(255, 255, 255, 255),
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                 elsnerLogo(),
+                const SizedBox(
+                  height: 100,
+                ),
+                const Text(
+                  "Welcome!\n\nLet's find you some services.",
+                  style: TextStyle(fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 100,
+                ),
+          // __________________________________________________________________________________________
+          // _________________________________Buttons__________________________________________________
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.pushReplacement(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => SignupScreen()));
-                    },
-                    child: Expanded(
-                      child: Container(
-                        alignment: Alignment.center,
-                        height: 50,
-                        width: 100,
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.25),
-                              blurRadius: 5,
-                              blurStyle: BlurStyle.normal,
-                              spreadRadius: 5,
-                              // offset: ()
-                            )
-                          ],
-                          color: const Color.fromARGB(255, 53, 49, 35),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: const Text(
-                          "Sign UP",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
+                              builder: (context) => const LoginScreen()),
+                        );
+                      },
+                      child: Expanded(
+                        child: Container(
+                          alignment: Alignment.center,
+                          height: 50,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.red.withOpacity(0.25),
+                                  blurRadius: 5,
+                                  blurStyle: BlurStyle.normal,
+                                  spreadRadius: 5)
+                            ],
+                            color: const Color.fromARGB(255, 255, 7, 7),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: Color.fromARGB(255, 255, 255, 255),
+                                fontWeight: FontWeight.bold),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-// __________________________________________________________________________________________
-// __________________________________________________________________________________________
-            ],
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SignupScreen()));
+                      },
+                      child: Expanded(
+                        child: Container(
+                          alignment: Alignment.center,
+                          height: 50,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.25),
+                                blurRadius: 5,
+                                blurStyle: BlurStyle.normal,
+                                spreadRadius: 5,
+                                // offset: ()
+                              )
+                            ],
+                            color: const Color.fromARGB(255, 53, 49, 35),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: const Text(
+                            "Sign UP",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+          // __________________________________________________________________________________________
+          // __________________________________________________________________________________________
+              ],
+            ),
           ),
         ),
       ),
@@ -178,3 +183,45 @@ class Welcome extends StatelessWidget {
 // ________________________________________________________________________________________
 // ________________________________________________________________________________________
 // ________________________________________________________________________________________
+Widget elsnerLogo() {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          "UN",
+          style: TextStyle(
+            shadows: [
+              Shadow(
+                  color: Color.fromARGB(255, 209, 140, 140),
+                  offset: Offset(1.0, 5.0),
+                  blurRadius: 5)
+            ],
+            decoration: TextDecoration.underline,
+            decorationColor: Colors.black,
+            decorationThickness: 2,
+            color: Colors.red,
+            fontSize: 50,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          "BIND",
+          style: TextStyle(
+            shadows: [
+              Shadow(
+                  color: Color.fromARGB(255, 109, 92, 92),
+                  offset: Offset(1, 5),
+                  blurRadius: 5)
+            ],
+            decoration: TextDecoration.overline,
+            decorationColor: Colors.red,
+            decorationThickness: 2,
+            color: Colors.black,
+            fontSize: 50,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
